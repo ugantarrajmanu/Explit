e6b291dffd6fccd93f0e0623c2a4ba0c4bfc7e15
ugantarrajmanu <ugantarrajmanu@gmail.com>
2025-12-20 02:38:10 +0530

    -fix next.config.js

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// next.config.js
-module.exports = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
 
 export default nextConfig;
